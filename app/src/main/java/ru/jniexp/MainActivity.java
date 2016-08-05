0041ae680bdb10bdc279ff772fff0ebd65d70060
winfe <winfected1992@gmail.com>
2016-08-05 15:32:23 +0300

    Add CPP

--- app/src/main/java/ru/jniexp/MainActivity.java
+++ app/src/main/java/ru/jniexp/MainActivity.java
@@ -5,9 +5,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static {
+        System.loadLibrary("nonograms_model");
+    }
+
+    public native String encrypt(String plainText);
+    public native String decrypt(String ciphertext);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String inputText = "Some Text";
+        String s1 = encrypt(inputText);
+        String s2 = decrypt(s1);
+        int x = 0;
     }
 }
--- app/src/main/jni/Engen.cpp
+++ app/src/main/jni/Engen.cpp
@@ -1,4 +1,29 @@
-//
-// Created by winfe on 04.08.2016.
-//
+#include <jni.h>
+#include "string"
+using namespace std;
 
+extern "C" JNIEXPORT jstring JNICALL
+Java_ru_jniexp_MainActivity_encrypt(JNIEnv *env, jobject instance, jstring plainText_) {
+    jboolean isCopy = JNI_TRUE;
+    const char *plainText = (env)->GetStringUTFChars(plainText_, &isCopy);
+    string result = "plane text";
+    if (isCopy == JNI_FALSE)
+        (env)->ReleaseStringUTFChars(plainText_, plainText);
+    else
+        delete plainText;
+    return (env)->NewStringUTF(result.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_ru_jniexp_MainActivity_decrypt(JNIEnv *env, jobject instance, jstring ciphertext_) {
+    jboolean isCopy = JNI_TRUE;
+    const char *ciphertext = env->GetStringUTFChars(ciphertext_, &isCopy);
+
+
+    if (isCopy == JNI_FALSE)
+        env->ReleaseStringUTFChars(ciphertext_, ciphertext);
+    else
+        delete ciphertext;
+
+    return env->NewStringUTF(ciphertext);
+}
